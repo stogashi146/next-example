80e237ec0aa4b275f9992609553d0dc7b70ac646
stogashi146 <stogashi146@gmail.com>
2024-04-25 18:20:45 +0900

    47 Loadingコンポーネントの実装

---
+++ next-tasks/src/app/(main)/loading.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <div
+      className="h-full flex justify-center items-center"
+      aria-label="読込中"
+    >
+      <div className="animate-spin h-10 w-10 border-4 border-green-500 rounded-full border-t-transparent"></div>
+      loading
+    </div>
+  );
+};
+
+export default loading;
